8eff3274f7fba7155bc25ff14b463b332b40afa2
Jules <jules.cibien@epitech.eu>
2022-10-06 23:38:21 +0200

    [add] front web

---
+++ front-web/src/Components/Dashboard/Dashboard_Services.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Dashboard.css"
+
+function Dashboard_Services() {
+    return (
+        <div className="DashboardServices">
+            <div className="ServicesContentTitle">
+                <p className="BasicServicesTitle">Basic Services</p>
+                <p className="VideoGamesTitle">Video Games</p>
+            </div>
+        </div>
+    )
+}
+
+export default Dashboard_Services
---
+++ front-web/src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './Sidebar.css';
+import { Link } from 'react-router-dom'
+import { SidebarContent } from './SidebarContent';
+import logo from '../../img/Sergify_Logo_Horizontal.png';
+
+function Sidebar() {
+    return (
+        <div className="Sidebar">
+            <div className="SidebarLogo">
+                <img src={logo} className="Logo"/>
+            </div>
+            <ul className="SidebarList">
+                {SidebarContent.map((val, key) => {
+                    return (
+                        <Link key={key} to={val.link}
+                        className="row"
+                        id={window.location.pathname === val.link ? "active" : ""}>
+                            <div id="icon">{val.icon}</div>
+                            <div id="title">{val.title}</div>
+                        </Link>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default Sidebar
