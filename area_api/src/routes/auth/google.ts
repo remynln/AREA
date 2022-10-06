edc60579d189c5df3f3a90f731c3ca26ebcf05a5
marcoReguillot <marco.reguillot@epitech.eu>
2022-10-06 10:51:29 +0200

    [ADD] oauth2 connection without custom redirect

--- area_api/src/routes/auth/google.ts
+++ area_api/src/routes/auth/google.ts
@@ -1,32 +1,57 @@
 import passport, { Strategy } from "passport";
 import express, { Request, Response, Router } from "express";
-const GoogleStrategy = require("passport-google-oidc")
+const GoogleStrategy = require("passport-google-oauth20")
 import "dotenv/config"
 
 var router: Router = express.Router()
 
-const strat = (callbackUrl: string): Strategy => new GoogleStrategy({
+router.use(passport.initialize())
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user: Express.User, done) => {
+    done(null, user);
+});
+
+passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: callbackUrl,
-  scope: ['profile']
-}, (issuer: any, profile: any, callback: any) => {
+  callbackURL: "http://localhost:8080/login/google/callback",
+}, function(accessToken: any, refresh_token: any, profile: any, callback: any) {
+    console.log("profile")
+    process.stdout.write(accessToken, refresh_token, profile)
     callback(null, {
-        name: "Charles-Rachid"
+        name: "user"
     })
+}))
+
+//router.get('/google', (req, res) => {
+//    console.log("azezea", req.query.callback)
+//    
+//    const callbackUrl: string = req.query.callback as string || ''
+//    if (callbackUrl == "") {
+//        res.status(403).json({
+//            message: "No callback url"
+//        })
+//        return;
+//    }
+//    passport.authenticate("google", (req, res) => {
+//        console.log("azd")
+//    })(req, res)
+//});
+
+router.get('/google', (req, res) => {
+    console.log("allé")
+    passport.authenticate("google", { scope: ['profile']})(req, res)
+}, (req, res) => {
+    console.log("nsm")
 })
 
-router.get('/google', (req, res, next) => {
-    console.log("azezea", req.query.callback)
-    
-    const callbackUrl: string = req.query.callback as string || ''
-    if (callbackUrl == "") {
-        res.status(403).json({
-            message: "No callback url"
-        })
-        return;
-    }
-    return passport.authenticate(strat(callbackUrl))(req, res, next)
-});
+router.get('/google/callback', passport.authenticate('google', {
+    failureRedirect: "http://localhost:8080/",
+    successRedirect: "http://localhost:8080/"
+}))
 
 export default router
