d79f3b87275153ab64b4050868d80809264f883d
Jules <jules.cibien@epitech.eu>
2022-10-07 17:31:51 +0200

    [up] Google service

--- front-web/src/Components/Dashboard/Dashboard_Services.js
+++ front-web/src/Components/Dashboard/Dashboard_Services.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Dashboard.css"
+import GoogleBackground from "../../img/Google_bg.jpg"
+import GoogleLogo from "../../img/Google_Logo.png"
 
 function Dashboard_Services() {
     return (
@@ -7,6 +9,13 @@ function Dashboard_Services() {
             <div className="ServicesContentTitle">
                 <p className="BasicServicesTitle">Basic Services</p>
                 <p className="VideoGamesTitle">Video Games</p>
+            </div>
+            <div className="Services">
+                <a href="http://localhost:8080/auth/service/google?callback=http://localhost:3000/dashboard">
+                    <div className="Service">
+                        <img src={GoogleLogo} id="logo"/>
+                    </div>
+                </a>    
             </div>
         </div>
     )
