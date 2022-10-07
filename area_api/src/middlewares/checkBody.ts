c09e2d777da9bdd12135c8a430f8984f38735cf8
marcoReguillot <marco.reguillot@epitech.eu>
2022-10-07 14:26:23 +0200

    [ADD] resgister route

--- area_api/src/middlewares/checkBody.ts
+++ area_api/src/middlewares/checkBody.ts
@@ -4,7 +4,7 @@ export default function checkBody(properties: Array<string>) {
     return (req: Request, res: Response, next: NextFunction) => {
         for (let i of properties) {
             if (!(i in req.body)) {
-                res.status(403).json({
+                res.status(400).json({
                     message: `missing '${i}' in body request`
                 })
             }
--- area_api/src/routes/auth/login.ts
+++ area_api/src/routes/auth/login.ts
@@ -4,8 +4,31 @@ import checkBody from '~/middlewares/checkBody'
 
 var auth: Router = Router()
 
-auth.post('/register',
-    checkBody(["email", "username", "password"]), (req: Request, res: Response) => {
+function isMailValid(email: string) {
+    return (/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email))
+}
+
+function isUserNameValid(username: string) {
+    return (/^[A-Z0-9._%+-]{2,}$/i.test(username))
+}
+
+function isPasswordValid(username: string) {
+    return (/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/i.test(username))
+}
+
+auth.post('/register', checkBody(["email", "username", "password"]), (req: Request, res: Response) => {
+    if (!isMailValid(req.body.email)) {
+        res.status(400).json({ message: "invalid email"})
+        return;
+    }
+    if (!isUserNameValid(req.body.username)) {
+        res.status(400).json({ message: "invalid username"})
+        return;
+    }
+    if (!isPasswordValid(req.body.password)) {
+        res.status(400).json({ message: "invalid password"})
+        return;
+    }
     res.status(200).json({
         nice : "ok"
     })
