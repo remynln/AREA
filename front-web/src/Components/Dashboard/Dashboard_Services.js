18d22d965b5e43c5583a6a81e9cf1ea53bc7d89a
Jules <jules.cibien@epitech.eu>
2022-10-08 15:11:56 +0200

    [up] Affichage des services

--- front-web/src/Components/Dashboard/Dashboard_Services.js
+++ front-web/src/Components/Dashboard/Dashboard_Services.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Dashboard.css"
-import GoogleBackground from "../../img/Google_bg.jpg"
-import GoogleLogo from "../../img/Google_Logo.png"
+import Services from "../Services/Services"
 
 function Dashboard_Services() {
     return (
@@ -10,13 +9,7 @@ function Dashboard_Services() {
                 <p className="BasicServicesTitle">Basic Services</p>
                 <p className="VideoGamesTitle">Video Games</p>
             </div>
-            <div className="Services">
-                <a href="http://localhost:8080/auth/service/google?callback=http://localhost:3000/dashboard">
-                    <div className="Service">
-                        <img src={GoogleLogo} id="logo"/>
-                    </div>
-                </a>    
-            </div>
+            <Services />
         </div>
     )
 }
---
+++ front-web/src/Components/Services/ServicesContent.js
@@ -0,0 +1,26 @@
+export const ServicesContent = [
+    {
+        link: "http://localhost:8080/auth/service/google?callback=http://localhost:3000/dashboard",
+        logo: "Twitter_Logo"
+    },
+    {
+        link: "http://localhost:8080/auth/service/google?callback=http://localhost:3000/dashboard",
+        logo: "Instagram_Logo"
+    },
+    {
+        link: "http://localhost:8080/auth/service/google?callback=http://localhost:3000/dashboard",
+        logo: "Google_Logo"
+    },
+    {
+        link: "http://localhost:8080/auth/service/google?callback=http://localhost:3000/dashboard",
+        logo: "Facebook_Logo"
+    },
+    {
+        link: "http://localhost:8080/auth/service/google?callback=http://localhost:3000/dashboard",
+        logo: "Github_Logo"
+    },
+    {
+        link: "http://localhost:8080/auth/service/google?callback=http://localhost:3000/dashboard",
+        logo: "Outlook_Logo"
+    }
+]
