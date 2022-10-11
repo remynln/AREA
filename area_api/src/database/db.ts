89c3308f4b12fe6b318e4a3748aa584481bbaa29
Remy <remy.noulin@epitech.eu>
2022-10-11 13:12:10 +0200

    [MOD] getToken and setToken

--- area_api/src/database/db.ts
+++ area_api/src/database/db.ts
@@ -1,9 +1,13 @@
 import Register from "./register";
 import Login from "./login";
+import SetToken from "./setToken";
+import GetToken from "./getToken";
 
 const db = {
     register:Register,
-    login:Login
+    login:Login,
+    setToken:SetToken,
+    getToken:GetToken
 }
 
 export default db
