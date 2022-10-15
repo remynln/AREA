feb47bbe95f6c6ffa1474e7fd9c7ce6a33455786
JohannSako <johann.sako@epitech.eu>
2022-10-16 01:10:55 +0200

    [ADD] slow slide up after launching page

---
+++ flutter/lib/front/slide_animation.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class AnimateSlide extends StatefulWidget {
+  const AnimateSlide({Key? key}) : super(key: key);
+
+  @override
+  State<AnimateSlide> createState() => _AnimateSlideState();
+}
+
+class _AnimateSlideState extends State<AnimateSlide>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _position;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 1000));
+    _position = Tween<double>(begin: 300, end: 0)
+        .animate(CurvedAnimation(parent: _controller, curve: Interval(0, 1)))
+      ..addListener(() {
+        setState(() {});
+      });
+    _controller.forward();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(top: _position.value),
+          child: Image.asset(
+            'assets/sergify.png',
+            height: 200,
+            width: 500,
+            fit: BoxFit.fitWidth,
+            scale: 1,
+          ),
+        ),
+      ],
+    );
+  }
+}
--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:area/front/slide_animation.dart';
 
 void main() => runApp(const MyApp());
 
@@ -37,119 +38,112 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-        body: Center(
-            child: ListView(
+      body: Center(
+          child: ListView(
       children: <Widget>[
-        Image.asset(
-          'assets/sergify.png',
-          height: 200,
-          width: 500,
-          fit: BoxFit.fitWidth,
-          scale: 1,
-        ),
-        SizedBox(height: 30),
-        Container(
-            alignment: Alignment.topCenter,
-            padding: const EdgeInsets.all(10),
-            child: const Text('Login',
-                style: TextStyle(
-                    fontSize: 35,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold))),
-        SizedBox(height: 10),
-        Container(
+      AnimateSlide(),
+      SizedBox(height: 30),
+      Container(
+          alignment: Alignment.topCenter,
           padding: const EdgeInsets.all(10),
-          child: TextField(
-            controller: nameController,
-            decoration: const InputDecoration(
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                labelText: 'User Name',
-                labelStyle: TextStyle(color: Colors.white)),
-          ),
-        ),
-        Container(
-          padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-          child: TextField(
-            obscureText: true,
-            controller: passwordController,
-            decoration: const InputDecoration(
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-                labelStyle: TextStyle(color: Colors.white)),
-          ),
-        ),
-        TextButton(
-          onPressed: () {
-            //forgot password screen
-          },
-          style: TextButton.styleFrom(
-            foregroundColor: Colors.red,
-          ),
-          child: const Text(
-            'Forgot Password',
-          ),
-        ),
-        Container(
-            height: 50,
-            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-            child: ElevatedButton(
-              child: const Text('Login'),
-              onPressed: () {
-                print(nameController.text);
-                print(passwordController.text);
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color.fromRGBO(191, 27, 44, 1),
+          child: const Text('Login',
+              style: TextStyle(
+                  fontSize: 35,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold))),
+      SizedBox(height: 10),
+      Container(
+        padding: const EdgeInsets.all(10),
+        child: TextField(
+          controller: nameController,
+          decoration: const InputDecoration(
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
               ),
-            )),
-        SizedBox(height: 30),
-        Container(
-            alignment: Alignment.topCenter,
-            child: const Text('Or continue with',
-                style: TextStyle(fontSize: 15, color: Colors.grey))),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 10.0),
-          child: ElevatedButton.icon(
-            onPressed: () {
-              print("google");
-            },
-            icon: Image.asset("assets/google.png", width: 18),
-            style: OutlinedButton.styleFrom(
-                side: BorderSide(
-                    width: 1.0, color: Color.fromRGBO(191, 27, 44, 1)),
-                backgroundColor: Colors.transparent),
-            label: Text('Google'), // <-- Text
-          ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              labelText: 'User Name',
+              labelStyle: TextStyle(color: Colors.white)),
         ),
-        ListTile(
-            title: Row(children: <Widget>[
-          Text("Don't have an account?",
-              style: TextStyle(fontSize: 15, color: Colors.grey)),
-          TextButton(
+      ),
+      Container(
+        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+        child: TextField(
+          obscureText: true,
+          controller: passwordController,
+          decoration: const InputDecoration(
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              border: OutlineInputBorder(),
+              labelText: 'Password',
+              labelStyle: TextStyle(color: Colors.white)),
+        ),
+      ),
+      TextButton(
+        onPressed: () {
+          //forgot password screen
+        },
+        style: TextButton.styleFrom(
+          foregroundColor: Colors.red,
+        ),
+        child: const Text(
+          'Forgot Password',
+        ),
+      ),
+      Container(
+          height: 50,
+          padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: ElevatedButton(
+            child: const Text('Login'),
             onPressed: () {
-              //forgot password screen
+              print(nameController.text);
+              print(passwordController.text);
             },
-            style: TextButton.styleFrom(
-              foregroundColor: Colors.white,
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color.fromRGBO(191, 27, 44, 1),
             ),
-            child: const Text(
-              'Create now',
+          )),
+      SizedBox(height: 30),
+      Container(
+          alignment: Alignment.topCenter,
+          child: const Text('Or continue with',
+              style: TextStyle(fontSize: 15, color: Colors.grey))),
+      Container(
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
+        child: ElevatedButton.icon(
+          onPressed: () {
+            print("google");
+          },
+          icon: Image.asset("assets/google.png", width: 18),
+          style: OutlinedButton.styleFrom(
+              side: BorderSide(
+                  width: 1.0, color: Color.fromRGBO(191, 27, 44, 1)),
+              backgroundColor: Colors.transparent),
+          label: Text('Google'), // <-- Text
+        ),
+      ),
+      ListTile(
+          title: Row(children: <Widget>[
+            Text("Don't have an account?",
+                style: TextStyle(fontSize: 15, color: Colors.grey)),
+            TextButton(
+              onPressed: () {
+                //forgot password screen
+              },
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+              ),
+              child: const Text(
+                'Create now',
+              ),
             ),
-          ),
-        ], mainAxisAlignment: MainAxisAlignment.center))
+          ], mainAxisAlignment: MainAxisAlignment.center))
       ],
     )));
   }
