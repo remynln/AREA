d39f4fd2157c212f2bda6730ff55538ad1448ebd
JohannSako <johann.sako@epitech.eu>
2022-10-16 01:14:48 +0200

    [ADD] opacity fading on sergify flutter icon

--- flutter/lib/front/slide_animation.dart
+++ flutter/lib/front/slide_animation.dart
@@ -11,13 +11,19 @@ class _AnimateSlideState extends State<AnimateSlide>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _position;
+  late Animation<double> _opacity;
 
   @override
   void initState() {
     _controller = AnimationController(
         vsync: this, duration: Duration(milliseconds: 1000));
-    _position = Tween<double>(begin: 300, end: 0)
+    _position = Tween<double>(begin: 100, end: 0)
         .animate(CurvedAnimation(parent: _controller, curve: Interval(0, 1)))
+      ..addListener(() {
+        setState(() {});
+      });
+    _opacity = Tween<double>(begin: 0, end: 1)
+        .animate(CurvedAnimation(parent: _controller, curve: Interval(0.5, 1)))
       ..addListener(() {
         setState(() {});
       });
@@ -32,12 +38,15 @@ class _AnimateSlideState extends State<AnimateSlide>
       children: [
         Padding(
           padding: EdgeInsets.only(top: _position.value),
-          child: Image.asset(
-            'assets/sergify.png',
-            height: 200,
-            width: 500,
-            fit: BoxFit.fitWidth,
-            scale: 1,
+          child: Opacity(
+            opacity: _opacity.value,
+            child: Image.asset(
+              'assets/sergify.png',
+              height: 200,
+              width: 500,
+              fit: BoxFit.fitWidth,
+              scale: 1,
+            ),
           ),
         ),
       ],
