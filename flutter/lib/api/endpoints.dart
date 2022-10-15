83688ea38e3d8c06484a2dec9d2f1f20b61b97f7
JohannSako <johann.sako@epitech.eu>
2022-10-15 18:39:54 +0200

    [ADD] api endpoits template for flutter

---
+++ flutter/lib/api/endpoints.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  static String baseUrl = '127.0.0.1:8080';
+  static String registerEndpoint = '/auth/register';
+  static String loginEndpoint = '/auth/login';
+  static String unregisterEndpoint = '/auth/unregister';
+  static String oauthEndpoint = '/auth/'; // put service next
+}
