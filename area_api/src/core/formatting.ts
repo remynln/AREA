12d8ffd5c9d55ca5bf8b44a1f648bf8f18cf5dba
marcoReguillot <marco.reguillot@epitech.eu>
2022-10-27 11:22:01 +0200

    [ADD] [NOT USED] working simple condition format

--- area_api/src/core/formatting.ts
+++ area_api/src/core/formatting.ts
@@ -44,3 +44,64 @@ export function formatContent(content: string, actionProperties: any) {
     }
     return content
 }
+
+
+
+// the conditions must be ordered by str length
+const CONDITION_OP_STR = {
+     '==': (v1: string, v2: string) => v1 == v2,
+     '!=': (v1: string, v2: string) => v1 != v2,
+     'in': (v1: string, v2: string) => v2.includes(v1),
+}
+
+const CONDITION_OP_NBR = {
+    '==': (v1: number, v2: number) => v1 == v2,
+    '!=': (v1: number, v2: number) => v1 != v2,
+    '<=': (v1: number, v2: number) => v1 <= v2,
+    '>=': (v1: number, v2: number) => v1 >= v2,
+    '<': (v1: number, v2: number) => v1 < v2,
+    '>': (v1: number, v2: number) => v1 > v2
+}
+
+function deleteQuotedThings(str: string, start: number = 0): string {
+    let quoteStart = str.indexOf('"', 0)
+    if (quoteStart == -1)
+        return str
+    let quoteEnd = str.indexOf('"', quoteStart + 1)
+    if (quoteEnd == -1)
+        throw Error(`Missing quote close in condition, at character ${quoteStart}`, { cause: "handled"})
+    return deleteQuotedThings(
+        str.substring(0, quoteStart) +
+        "_".repeat(1 + quoteEnd - quoteStart) +
+        str.substring(quoteEnd + 1)
+    )
+}
+
+export function checkSimpleCondition(condition: string, substringIndex: number, actionProperties: any) {
+    var type: string | undefined = undefined
+    var deletedQuote = deleteQuotedThings(condition)
+    var operatorPos: number = -1
+    var operator: string | undefined = undefined
+
+    for (let i in CONDITION_OP_NBR) {
+        operatorPos = deletedQuote.indexOf(i)
+        if (operatorPos != -1) {
+            type = 'number'
+            operator = i
+            break;
+        }
+    }
+    if (!type || !operator) {
+        for (let i in CONDITION_OP_STR) {
+            operatorPos = deletedQuote.indexOf(i)
+            if (operatorPos != -1) {
+                type = 'number'
+                operator = i
+                break;
+            }
+        }
+    }
+    if (!type || !operator)
+        throw Error(`No conditional operator, at character ${substringIndex}`, { cause: "handled" })
+    console.log(operator)
+}
