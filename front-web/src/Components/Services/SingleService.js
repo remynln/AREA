11bb75cb0efbf1faddca0379391fabb96fbc89fe
Jules <jules.cibien@epitech.eu>
2022-10-31 19:05:23 +0100

    [up] IP dynamique, Services avec état fonctionnel

--- front-web/src/Components/Services/SingleService.js
+++ front-web/src/Components/Services/SingleService.js
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./Services.css"
 
 function SingleService(props) {
+    const [isConnected, setConnection] = useState(JSON.parse(localStorage.getItem(props.name)))
+
+    useEffect(() => {
+        setConnection(JSON.parse(localStorage.getItem(props.name)))
+    }, [localStorage.getItem(props.name)])
+
     let Disconnected = {
         border: "3px solid #4B7844",
+        pointerEvents: "none"
+    };
+    
+    let Connected = {
+        border: "3px solid #BF1B2C",
         pointerEvents: "auto"
     };
 
-    let Connected = {
-        border: "3px solid #BF1B2C",
-        pointerEvents: "none"
-    };
-
     return (
-        <a className="SingleService" href={props.link} style={JSON.parse(localStorage.getItem(props.name))==="connected"?Connected:Disconnected}>
+        <a className="SingleService" href={process.env.REACT_APP_SERVER_IP + "/service/" + props.name + "?callback=" + process.env.REACT_APP_FRONT_IP + "/dashboard"} style={isConnected==="connected"?Disconnected:Connected}>
             <div className="ServiceBgLogo">
                 <div className="ServiceBg">
                     <img src={require("../../img/" + props.logo + ".png")} class="ServiceLogo" alt="Service_Logo"/>
                 </div>
             </div>
-            <div className="ServiceStateTextBg" style={JSON.parse(localStorage.getItem(props.name))==="connected"?{backgroundColor: "#BF1B2C"}:{backgroundColor: "#4B7844"}}>
-                <p className="ServiceStateText">{JSON.parse(localStorage.getItem(props.name))==="connected"?"DISCONNECT":"CONNECT"}</p>
+            <div className="ServiceStateTextBg" style={isConnected==="connected"?{backgroundColor: "#4B7844"}:{backgroundColor: "#BF1B2C"}}>
+                <p className="ServiceStateText">{isConnected==="connected"?"CONNECTED":"DISCONNECTED"}</p>
             </div>
         </a>
     )
