386d3377ae0afb1f3e6fd411db72d01a4cb5753f
JohannSako <johann.sako@epitech.eu>
2022-11-01 23:50:30 +0100

    [ADD] basic background for create page

--- flutter/lib/front/standard_pages/pages.dart
+++ flutter/lib/front/standard_pages/pages.dart
@@ -4,12 +4,14 @@ import 'package:area/front/standard_pages/dashboard.dart';
 import 'package:area/front/standard_pages/settings.dart';
 import 'package:area/front/standard_pages/notification.dart';
 import 'package:area/front/standard_pages/workflows.dart';
+import 'package:area/front/standard_pages/create.dart';
 
 class PagesWidget extends StatefulWidget {
   final String token;
   final int index;
 
-  const PagesWidget({Key? key, required this.token, required this.index}) : super(key: key);
+  const PagesWidget({Key? key, required this.token, required this.index})
+      : super(key: key);
 
   @override
   State<PagesWidget> createState() => _PagesWidgetState();
@@ -21,6 +23,7 @@ class _PagesWidgetState extends State<PagesWidget> {
 
   late DashboardWidget dashboard;
   late NotificationWidget notification;
+  late CreateWidget create;
   late WorkflowsWidget workflows;
   late SettingsWidget settings;
   static List<Widget> _pagesWidget = <Widget>[];
@@ -32,55 +35,54 @@ class _PagesWidgetState extends State<PagesWidget> {
     _index = widget.index;
     dashboard = DashboardWidget(token: token);
     notification = const NotificationWidget();
+    create = CreateWidget(token: token);
     workflows = const WorkflowsWidget();
     settings = const SettingsWidget();
-    _pagesWidget = [
-      dashboard,
-      notification,
-      workflows,
-      settings
-    ];
+    _pagesWidget = [dashboard, notification, create, workflows, settings];
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: _pagesWidget[_index]),
-      bottomNavigationBar: Container(
-        decoration: const BoxDecoration(
-          boxShadow: <BoxShadow>[
+        body: Center(child: _pagesWidget[_index]),
+        bottomNavigationBar: Container(
+          decoration: const BoxDecoration(boxShadow: <BoxShadow>[
             BoxShadow(
-              color: Colors.white,
-              blurRadius: 0.5,
-              blurStyle: BlurStyle.solid
-            )
-          ]
-        ),
-        child: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-                icon: Icon(Icons.dashboard_outlined), label: 'Dashboard'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.notifications_none), label: 'Notification'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.bookmark_border_rounded), label: 'Workflows'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.settings_outlined), label: 'Settings'),
-          ],
-          type: BottomNavigationBarType.fixed,
-          selectedItemColor: const Color.fromRGBO(238, 13, 36, 100),
-          unselectedItemColor: Colors.white,
-          backgroundColor: Colors.black,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          currentIndex: _index,
-          onTap: (int index) {
-            setState(() {
-              _index = index;
-            });
-          },
-        ),
-      )
-    );
+                color: Colors.white,
+                blurRadius: 0.5,
+                blurStyle: BlurStyle.solid)
+          ]),
+          child: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.dashboard_outlined), label: 'Dashboard'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.notifications_none), label: 'Notification'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.add_box_rounded,
+                      color: Colors.amber, size: 40),
+                  activeIcon: Icon(Icons.add_box_rounded, size: 40),
+                  label: 'Create'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.bookmark_border_rounded),
+                  label: 'Workflows'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.settings_outlined), label: 'Settings'),
+            ],
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: const Color.fromRGBO(238, 13, 36, 100),
+            unselectedItemColor: Colors.white,
+            backgroundColor: Colors.black,
+            iconSize: 20,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            currentIndex: _index,
+            onTap: (int index) {
+              setState(() {
+                _index = index;
+              });
+            },
+          ),
+        ));
   }
 }
