a1bfa7fbcff664f57b0ef008c3c23321cf73a8a9
JohannSako <johann.sako@epitech.eu>
2022-11-01 17:58:36 +0100

    [UPDATE] fix merge conflic in flutter part keep dashboard changes

--- flutter/lib/api/endpoints.dart
+++ flutter/lib/api/endpoints.dart
@@ -5,4 +5,18 @@ class ApiConstants {
   static String loginEndpoint = '/auth/login';
   static String googleLoginEndpoint = '/auth/service/google';
   static String unregisterEndpoint = '/auth/unregister';
+  static String createEndpoint = '/area/create';
+  static String servicesEndpoint = '/services';
+  static String actionsEndpoint(String service_name) {
+    return "/service/$service_name/actions";
+  }
+  static String reactionsEndpoint(String service_name) {
+    return "/service/$service_name/reactions";
+  }
+  static String actionEndpoint(String service_name, String action_name) {
+    return "/service/$service_name/action/$action_name";
+  }
+  static String reactionEndpoint(String service_name, String reaction_name) {
+    return "/service/$service_name/reaction/$reaction_name";
+  }
 }
