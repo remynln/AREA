da39c71eaee45d9e9349947c76afa9d902a103b0
JohannSako <johann.sako@epitech.eu>
2022-11-03 11:52:50 +0100

    [ADD] functionnal action service

--- flutter/lib/front/standard_pages/create.dart
+++ flutter/lib/front/standard_pages/create.dart
@@ -2,6 +2,10 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
+import 'package:area/api/service/services.dart';
+import 'package:area/api/connection.dart';
+import 'package:area/api/answer/services_answer.dart';
+
 import 'package:area/front/standard_pages/create_popup/action_service.dart';
 import 'package:area/front/standard_pages/create_popup/action_trigger.dart';
 import 'package:area/front/standard_pages/create_popup/reaction_service.dart';
@@ -17,11 +21,128 @@ class CreateWidget extends StatefulWidget {
 }
 
 class _CreateWidgetState extends State<CreateWidget> {
-  bool _actionTrigger = false;
-  bool _reactionService = false;
-  bool _reactionTrigger = false;
+  Service _actionService = Service("", "", "", []);
+  Service _actionTrigger = Service("", "", "", []);
+  Service _reactionService = Service("", "", "", []);
+  Service _reactionTrigger = Service("", "", "", []);
 
-  Container displayButton(String title, bool isTrigger, Function function) {
+  Color switchColor() {
+    if (_actionService.name.isEmpty) {
+      return (Colors.grey);
+    } else {
+      return (const Color.fromRGBO(191, 27, 44, 1));
+    }
+  }
+
+  Widget displayActionServices(
+      ServicesAnswer? servicesAnswer, context, bool changed, setState, setStateWidget) {
+    List<Widget> list = [];
+    List<Service> allServices = Services.BasicServices + Services.GamesServices;
+
+    if (servicesAnswer == null) {
+      return Container();
+    }
+    for (var index = 0; index < allServices.length; index++) {
+      if (servicesAnswer.connected.contains(allServices[index].name) == false) {
+        continue;
+      }
+      list.add(GestureDetector(
+          onTap: () {
+            changed = !changed;
+            _actionService.name = allServices[index].name;
+            setState(() {});
+          },
+          child: allServices[index].name == _actionService.name
+              ? Image.asset(allServices[index].not_connected_image)
+              : Image.asset(allServices[index].connected_image,
+                  filterQuality: FilterQuality.high)));
+      list.add(const SizedBox(height: 20));
+    }
+    list.add(Container(
+      height: 60,
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        onPressed: () {
+          if (_actionService.name.isNotEmpty) {
+            _actionService = allServices
+                .firstWhere((element) => _actionService.name == element.name);
+            setStateWidget((){});
+          } else
+            return;
+          Navigator.of(context).pop();
+        },
+        style: ElevatedButton.styleFrom(backgroundColor: switchColor()),
+        child: const Text(
+          "CONFIRM",
+        ),
+      ),
+    ));
+    return (Column(children: list));
+  }
+
+  Widget getActionServices(context, bool changed, setState, setStateWidget) {
+    return FutureBuilder(
+      future: ApiService().getConnectedServices(widget.token),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: displayActionServices(
+                  snapshot.data, context, changed, setState, setStateWidget));
+        } else {
+          return Container(
+              width: MediaQuery.of(context).size.width / 1.8,
+              height: MediaQuery.of(context).size.height / 2,
+              child: Center(
+                  child: CircularProgressIndicator(color: Colors.white)));
+        }
+      },
+    );
+  }
+
+  void openActionService(context, StateSetter setStateWidget) {
+    bool changed = false;
+
+    if (_actionService.name.isNotEmpty &&
+        _actionService.not_connected_image.isEmpty) {
+      _actionService.name = "";
+    }
+    showDialog(
+        context: context,
+        builder: (context) {
+          return StatefulBuilder(builder: (builder, setState) {
+            return AlertDialog(
+                backgroundColor: Color.fromRGBO(60, 60, 60, 1),
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      30,
+                    ),
+                  ),
+                ),
+                title: const Text(
+                  "Action's Service",
+                  style: TextStyle(
+                      fontSize: 23,
+                      color: Colors.white,
+                      fontFamily: "RobotoMono",
+                      fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                content: SizedBox(
+                    height: 500,
+                    width: 600,
+                    child: Padding(
+                        padding: const EdgeInsetsDirectional.only(start: 20),
+                        child: Row(children: <Widget>[
+                          getActionServices(context, changed, setState, setStateWidget)
+                        ]))));
+          });
+        });
+  }
+
+  Container displayButton(
+      String title, String image, bool isTrigger, Function function) {
     return Container(
       padding: EdgeInsets.only(left: 20, right: 20, bottom: 40),
       height: 270.0,
@@ -42,20 +163,19 @@ class _CreateWidgetState extends State<CreateWidget> {
                           fontFamily: "RobotoMono",
                           fontWeight: FontWeight.bold))),
               SizedBox(height: 20),
-              ElevatedButton(
-                  onPressed: () {
-                    function();
-                  },
-                  style: ButtonStyle(
-                      fixedSize:
-                          const MaterialStatePropertyAll<Size>(Size(250, 120)),
-                      backgroundColor: const MaterialStatePropertyAll<Color>(
-                          Color.fromRGBO(80, 80, 80, 100)),
-                      shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(24),
-                              side: const BorderSide(color: Colors.black)))),
-                  child: const Icon(Icons.add, size: 30))
+              RawMaterialButton(
+                onPressed: () {
+                  function();
+                },
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24),
+                    side: const BorderSide(color: Colors.black)),
+                fillColor: const Color.fromRGBO(80, 80, 80, 100),
+                child: image.isEmpty
+                    ? const Icon(Icons.add, size: 30)
+                    : Image.asset(image),
+                constraints: BoxConstraints(minWidth: 230, minHeight: 120),
+              )
             ],
           )),
     );
@@ -75,31 +195,27 @@ class _CreateWidgetState extends State<CreateWidget> {
                   fontFamily: "RobotoMono",
                   fontWeight: FontWeight.bold))),
       SizedBox(height: 50),
-      displayButton("Action's Service", false, () {
-        openActionService(context, widget.token);
-        setState(() {
-          _actionTrigger = _actionTrigger ? false : true;
-        });
+      displayButton(
+          "Action's Service", _actionService.not_connected_image, false, () {
+        openActionService(context, setState);
       }),
-      _actionTrigger
-          ? displayButton("Action's Trigger", true, () {
+      _actionService.not_connected_image.isNotEmpty
+          ? displayButton(
+              "Action's Trigger", _actionTrigger.not_connected_image, true, () {
               openActionTrigger(context);
-
-              setState(() {
-                _reactionService = _reactionService ? false : true;
-              });
             })
           : Container(),
-      _reactionService
-          ? displayButton("Reaction's Service", true, () {
+      _actionTrigger.not_connected_image.isNotEmpty
+          ? displayButton(
+              "Reaction's Service", _reactionService.not_connected_image, true,
+              () {
               openReactionService(context);
-              setState(() {
-                _reactionTrigger = _reactionTrigger ? false : true;
-              });
             })
           : Container(),
-      _reactionTrigger
-          ? displayButton("Reaction's Trigger", true, () {
+      _reactionService.not_connected_image.isNotEmpty
+          ? displayButton(
+              "Reaction's Trigger", _reactionTrigger.not_connected_image, true,
+              () {
               openReactionTrigger(context);
             })
           : Container()
