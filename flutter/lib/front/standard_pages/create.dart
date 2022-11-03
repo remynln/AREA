de93127781f07c96ed54060b50eb7517bc4bf7c7
JohannSako <johann.sako@epitech.eu>
2022-11-03 19:33:39 +0100

    [ADD] temporary form action

--- flutter/lib/front/standard_pages/create.dart
+++ flutter/lib/front/standard_pages/create.dart
@@ -1,12 +1,17 @@
+import 'dart:convert';
 import 'dart:ui';
 
-import 'package:area/api/answer/actions_answer.dart';
 import 'package:flutter/material.dart';
 
 import 'package:area/api/service/services.dart';
 import 'package:area/api/connection.dart';
 import 'package:area/api/answer/services_answer.dart';
+import 'package:area/api/answer/actions_answer.dart';
 import 'package:area/api/area.dart';
+
+import 'package:simple_form_builder/formbuilder.dart';
+import 'package:simple_form_builder/global/checklistModel.dart';
+import 'package:simple_form_builder/global/constant.dart';
 
 class CreateWidget extends StatefulWidget {
   final String token;
@@ -19,12 +24,13 @@ class CreateWidget extends StatefulWidget {
 
 class _CreateWidgetState extends State<CreateWidget> {
   Service _actionService = Service("", "", "", []);
-  ActionService _actionTrigger = ActionService("", "", {}, {});
+  ActionsAnswer _actionTrigger = ActionsAnswer();
+  String _condition = "";
   Service _reactionService = Service("", "", "", []);
   ReactionService _reactionTrigger = ReactionService("", "", {});
 
-  Color switchColor(Service service) {
-    if (service.name.isEmpty) {
+  Color switchColor(String name) {
+    if (name.isEmpty) {
       return (Colors.grey);
     } else {
       return (const Color.fromRGBO(191, 27, 44, 1));
@@ -69,7 +75,7 @@ class _CreateWidgetState extends State<CreateWidget> {
           Navigator.of(context).pop();
         },
         style: ElevatedButton.styleFrom(
-            backgroundColor: switchColor(_actionService)),
+            backgroundColor: switchColor(_actionService.name)),
         child: const Text(
           "CONFIRM",
         ),
@@ -137,22 +143,82 @@ class _CreateWidgetState extends State<CreateWidget> {
         });
   }
 
-  Widget displayActionTriggers(List<ActionsAnswer>? actionsAnswer, context, setState, setStateWidget) {
+  Widget getTriggerText(String name, String description, Color color) {
+    return (Container(
+        decoration: BoxDecoration(
+            border: Border.all(color: color),
+            color: const Color.fromRGBO(80, 80, 80, 100),
+            borderRadius: BorderRadius.circular(24)),
+        child: Column(children: <Widget>[
+          Text(name,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: "Roboto",
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20)),
+          SizedBox(height: 10),
+          Text(description,
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontFamily: "Roboto"))
+        ])));
+  }
+
+  Widget displayActionTriggers(
+      List<ActionsAnswer>? actionsAnswer, context, setState, setStateWidget) {
+    List<Widget> list = [];
+
     if (actionsAnswer == null) {
       return Container();
     }
-    for (ActionsAnswer answer in actionsAnswer!) {
-      print(answer.name);
-      print(answer.description);
-      print(answer.parameters);
-      print(answer.properties);
+
+    for (var index = 0; index < actionsAnswer.length; index++) {
+      list.add(SizedBox(
+          width: 223,
+          child: GestureDetector(
+              onTap: () {
+                _actionTrigger.name = actionsAnswer[index].name;
+                setState(() {});
+              },
+              child: actionsAnswer[index].name == _actionTrigger.name
+                  ? getTriggerText(
+                      actionsAnswer[index].name,
+                      actionsAnswer[index].description,
+                      const Color.fromRGBO(191, 27, 44, 1))
+                  : getTriggerText(
+                      actionsAnswer[index].name,
+                      actionsAnswer[index].description,
+                      const Color.fromRGBO(62, 149, 49, 1)))));
+      list.add(const SizedBox(height: 20));
     }
-    return (const Text("HEY", style: TextStyle(color: Colors.white)));
+    list.add(Container(
+      height: 60,
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        onPressed: () {
+          if (_actionTrigger.name.isNotEmpty) {
+            _actionTrigger = actionsAnswer
+                .firstWhere((element) => _actionTrigger.name == element.name);
+            setStateWidget(() {});
+          } else {
+            return;
+          }
+          Navigator.of(context).pop();
+        },
+        style: ElevatedButton.styleFrom(
+            backgroundColor: switchColor(_actionTrigger.name)),
+        child: const Text(
+          "CONFIRM",
+        ),
+      ),
+    ));
+    return (Column(children: list));
   }
 
   Widget getActionTriggers(context, setState, setStateWidget) {
     return FutureBuilder(
-      future: ApiService().getActionsFromService(widget.token, _actionService.name),
+      future:
+          ApiService().getActionsFromService(widget.token, _actionService.name),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           return SingleChildScrollView(
@@ -286,15 +352,24 @@ class _CreateWidgetState extends State<CreateWidget> {
         });
   }
 
+  StatelessWidget displayTriggerName(String title) {
+    if (title.contains("Action") && _actionTrigger.description.isNotEmpty) {
+      return (Text(_actionTrigger.name, style: const TextStyle(color: Colors.white, fontFamily: "Roboto", fontWeight: FontWeight.bold, fontSize: 18)));
+    } else if (title.contains("Reaction") && _reactionTrigger.description.isNotEmpty) {
+      return (Text(_reactionTrigger.name, style: const TextStyle(color: Colors.white, fontFamily: "Roboto", fontWeight: FontWeight.bold, fontSize: 18)));
+    }
+    return (Container());
+  }
+
   Container displayButton(
       String title, String image, bool isTrigger, Function function) {
     return Container(
-      padding: EdgeInsets.only(left: 20, right: 20, bottom: 40),
+      padding: const EdgeInsets.only(left: 20, right: 20, bottom: 40),
       height: 270.0,
       width: 100.0,
       color: Colors.transparent,
       child: Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
               color: Color.fromRGBO(100, 100, 100, 100),
               borderRadius: BorderRadius.all(Radius.circular(24))),
           child: Column(
@@ -302,12 +377,12 @@ class _CreateWidgetState extends State<CreateWidget> {
               Padding(
                   padding: EdgeInsets.only(top: 10),
                   child: Text(title,
-                      style: TextStyle(
+                      style: const TextStyle(
                           fontSize: 23,
                           color: Colors.white,
                           fontFamily: "RobotoMono",
                           fontWeight: FontWeight.bold))),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               RawMaterialButton(
                 onPressed: () {
                   function();
@@ -316,12 +391,94 @@ class _CreateWidgetState extends State<CreateWidget> {
                     borderRadius: BorderRadius.circular(24),
                     side: const BorderSide(color: Colors.black)),
                 fillColor: const Color.fromRGBO(80, 80, 80, 100),
+                constraints: const BoxConstraints(minWidth: 230, minHeight: 120),
                 child: image.isEmpty
                     ? const Icon(Icons.add, size: 30)
                     : Image.asset(image),
-                constraints: BoxConstraints(minWidth: 230, minHeight: 120),
-              )
+              ),
+              const SizedBox(height: 10),
+              isTrigger ? displayTriggerName(title) : Container()
             ],
+          )),
+    );
+  }
+
+  List<Widget> displayActionDetail() {
+    _actionTrigger.parameters.forEach((key, value) {
+      print(key);
+      print(value);
+    });
+    _actionTrigger.properties.forEach((key, value) {
+      print(key);
+      print(value);
+      if (value.runtimeType.toString() == "_InternalLinkedHashMap<String, dynamic>") {
+        print("MAP");
+      }
+      else
+        print("OTHER");
+    });
+    Map<String, dynamic> data = {
+      "status": 1,
+      "data": [
+        {
+          "questions": [
+            {
+              "question_id": String,
+              "fields": [],
+              "_id": "60dc6a3dc9fe14577c30d271",
+              "title": "Parameter",
+              "description": "",
+              "remark": false,
+              "type": "text",
+              "is_mandatory": true
+            }
+          ]
+        }
+      ]
+    };
+    return (<Widget>[
+      FormBuilder(
+          initialData: data,
+          index: 0,
+          submitButtonWidth: 0.5,
+          submitButtonDecoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          showIcon: false,
+          onSubmit: (ChecklistModel val) {
+            print(val.data?.first.questions?.first.answer);
+            _actionTrigger.detail = true;
+          })
+    ]);
+  }
+
+  List<Widget> displayReactionDetail() {
+    return (<Widget>[]);
+  }
+
+  Container displayDetail(String title, bool isAction) {
+    return Container(
+      constraints: BoxConstraints(minHeight: 270.0),
+      padding: EdgeInsets.only(left: 20, right: 20, bottom: 40),
+      width: 100.0,
+      color: Colors.transparent,
+      child: Container(
+          decoration: const BoxDecoration(
+              color: Color.fromRGBO(100, 100, 100, 100),
+              borderRadius: BorderRadius.all(Radius.circular(24))),
+          child: Column(
+            children: <Widget>[
+                  Padding(
+                      padding: EdgeInsets.only(top: 10),
+                      child: Text(title,
+                          style: const TextStyle(
+                              fontSize: 23,
+                              color: Colors.white,
+                              fontFamily: "RobotoMono",
+                              fontWeight: FontWeight.bold)))
+                ] +
+                (isAction ? displayActionDetail() : displayReactionDetail()),
           )),
     );
   }
@@ -351,6 +508,9 @@ class _CreateWidgetState extends State<CreateWidget> {
             })
           : Container(),
       _actionTrigger.description.isNotEmpty
+          ? displayDetail("Action's Detail", true)
+          : Container(),
+      _actionTrigger.detail
           ? displayButton(
               "Reaction's Service", _reactionService.not_connected_image, true,
               () {
