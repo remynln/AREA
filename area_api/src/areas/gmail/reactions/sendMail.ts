751d650f4e2d6990d74c98858031e965eebf48f7
marcoReguillot <marco.reguillot@epitech.eu>
2022-11-03 22:06:47 +0100

    [ADD] clean error handling

--- area_api/src/areas/gmail/reactions/sendMail.ts
+++ area_api/src/areas/gmail/reactions/sendMail.ts
@@ -12,40 +12,43 @@ const sendMail: Reaction = {
         'object': 'string',
         'body': 'string'
     },
-    async launch(params, token) {
+    async launch(params, token, refresh) {
         console.log("sending mail...")
         const msg = createMimeMessage()
-        let mail: string
-        try {
-            mail = await getMailFromToken(token);
-        } catch (e: any) {
-            if (!e.response)
-                throw e
-            let err = e as AxiosError
-            if (err.response?.status == 401)
-                return AreaRet.AccessTokenExpired
-            else
-                throw e
-        }
+        let mail: string = await refresh(async () => {
+            try {
+                let newMail = await getMailFromToken(token);
+                return newMail
+            } catch (e: any) {
+                if (!e.response)
+                    throw e
+                let err = e as AxiosError
+                if (err.response?.status == 401)
+                    return AreaRet.AccessTokenExpired
+                else
+                    throw e
+            }
+        })
         msg.setSender({name: 'Marco', addr: mail})
         msg.setRecipient(params.recipient)
         msg.setSubject(params.object)
         msg.setMessage('text/plain', params.body)
-        try {
-            await axios.post("https://gmail.googleapis.com/upload/gmail/v1/users/me/messages/send",
-                msg.asRaw(), {
-                headers: {
-                    'Content-Type': 'message/rfc822',
-                    'Authorization': 'Bearer ' + token
+        refresh(async () => {
+            try {
+                await axios.post("https://gmail.googleapis.com/upload/gmail/v1/users/me/messages/send",
+                    msg.asRaw(), {
+                    headers: {
+                        'Content-Type': 'message/rfc822',
+                        'Authorization': 'Bearer ' + token
+                    }
+                })
+            } catch (err: any) {
+                if (err.response.status == 401) {
+                    return AreaRet.AccessTokenExpired
                 }
-            })
-        } catch (err: any) {
-            if (err.response.status == 401) {
-                return AreaRet.AccessTokenExpired
+                throw err
             }
-            throw err
-        }
-        return AreaRet.Ok
+        })
     }
 }
 
