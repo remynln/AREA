58d66b484a34ab7778aca84f1a5b446947e353de
marcoReguillot <marco.reguillot@epitech.eu>
2022-11-05 17:46:06 +0100

    [ADD] area lock in function of services

--- area_api/src/routes/user/areas.ts
+++ area_api/src/routes/user/areas.ts
@@ -1,19 +1,21 @@
 import { Router } from "express";
+import AreaInstances from "~/core/instances";
 import db from "~/database/db";
 
 
 var areasRouter = Router({mergeParams: true})
 
 areasRouter.get('/', (req, res, next) => {
-    db.area.getFromUser(res.locals.targetUser).then((areas) => {
+    AreaInstances.list(res.locals.targetUser).then((areas) => {
         let mapped = areas.map((item) => {
             return {
-                id: item._id.toHexString(),
-                title: item.title,
-                description: item.description,
-                action: item.action,
-                condition: item.condition,
-                reaction: item.reaction
+                id: item[0],
+                title: item[1].title,
+                description: item[1].description,
+                action: item[1].action,
+                condition: item[1].condition,
+                reaction: item[1].reaction,
+                status: item[1].status
             }
         })
         res.status(200).json(mapped)
