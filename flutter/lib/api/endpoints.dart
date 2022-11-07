e13c7a12a7245808294887b0f1368763f9136e12
JohannSako <johann.sako@epitech.eu>
2022-11-07 16:56:37 +0100

    [ADD] user, admin and areas endpoints

--- flutter/lib/api/endpoints.dart
+++ flutter/lib/api/endpoints.dart
@@ -7,6 +7,7 @@ class ApiConstants {
   static String unregisterEndpoint = '/auth/unregister';
   static String createEndpoint = '/area/create';
   static String servicesEndpoint = '/services';
+  static String usersEndpoint = '/users';
   static String serviceEndpoint(String service_name) {
     return "/service/$service_name";
   }
@@ -21,5 +22,20 @@ class ApiConstants {
   }
   static String reactionEndpoint(String service_name, String reaction_name) {
     return "/service/$service_name/reaction/$reaction_name";
+  }
+  static String userAreasEndpoint({String user_id = "me"}) {
+    return "/user/$user_id/areas";
+  }
+  static String areaEndpoint(String area_id) {
+    return "/area/$area_id";
+  }
+  static String disableAreaEndpoint(String area_id) {
+    return "/area/$area_id/disable";
+  }
+  static String enableAreaEndpoint(String area_id) {
+    return "/area/$area_id/enable";
+  }
+  static String userEndpoint(String user_id) {
+    return "/user/$user_id";
   }
 }
