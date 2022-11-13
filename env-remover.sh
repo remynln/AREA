a742c515a636b752b49182980cb64ecf3f50d1f8
Remy <remy.noulin@epitech.eu>
2022-11-13 02:40:47 +0100

    [MOD] env-remover

--- env-remover.sh
+++ env-remover.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
-if [[ -z $1 ]];then
-	echo "Require at least one argument, check help"
-	exit 1
-fi
-if [[ $1 == "-h" ]];then
-	echo "./env-remover.sh \"<path to the env file>\" -f (optional force everything)"
-	exit 1
-fi
-filepath=$1
-remote=$(git config --get remote.origin.url)
-branch=$(git rev-parse --abbrev-ref HEAD)
-if [[ -z $2 ]];then
-	git filter-repo --path $filepath --invert-paths
-	git remote add origin $remote
-	git push --set-upstream origin $branch --force
-elif [[ $2 == "-f" ]];then
-	git filter-repo --path $filepath --invert-paths --force
+function remove_env() {
+    if [[ -z $1 ]];then
+        echo "Require at least one argument, check help"
+        exit 1
+    fi
+    if [[ $1 == "-h" ]];then
+        echo "./env-remover.sh \"<path to the env file>\" -f (optional force everything)"
+        exit 1
+    fi
+    filepath=$1
+    remote=$(git config --get remote.origin.url)
+    branch=$(git rev-parse --abbrev-ref HEAD)
+    if [[ -z $2 ]];then
+        git filter-repo --path $filepath --invert-paths
         git remote add origin $remote
         git push --set-upstream origin $branch --force
-else
-	echo "Bad syntax"
-	exit 1
-fi
-# dedicace a marco
+    elif [[ $2 == "-f" ]];then
+        git filter-repo --path $filepath --invert-paths --force
+            git remote add origin $remote
+            git push --set-upstream origin $branch --force
+    else
+        echo "Bad syntax"
+        exit 1
+    fi
+}
+
+
+for branch in $(git for-each-ref --format='%(refname:short)'); do
+    true_branch_name=$(echo $branch | sed -e 's/\origin\///g')
+    echo "Branch name:  $true_branch_name"
+    echo "checkouting"
+    git checkout $true_branch_name > /dev/null
+    echo "Actual branch: $(git rev-parse --abbrev-ref HEAD)"
+    echo "Finding .env (excluding front web)"
+    env_file=$(find . -type f -name ".env" ! -path "./front-web/*" ! -path "./area_api/node_modules/*")
+    if [[ -z $env_file ]]; then
+        echo "$env_file in $true_branch_name"
+    fi
+done
